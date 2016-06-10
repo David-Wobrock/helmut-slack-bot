28a3199402b693d7866943a358167d35c4660682
David-Wobrock <david.wobrock@gmail.com>
2016-06-10 18:24:45 +0200

    first try with botkit

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-var RtmClient = require('@slack/client').RtmClient;
-var token = 'xoxb-49895947781-WXerjwLI6CpltOcziC7RU6ze';
+/*var rtmClient = require('@slack/client').RtmClient;
+var token = process.env.SLACK_API_TOKEN || '';
 var rtm = new RtmClient(token, {logLevel: 'debug'});
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
 	console.log(message);
 });
-rtm.start();
+rtm.start();*/
+
+if (!process.env.token) {
+}
+
+var Botkit = require('Botkit');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: true,
+});
+
+var bot = controller.spawn({
+    token: process.env.token || 'xoxb-49895947781-WXerjwLI6CpltOcziC7RU6ze'
+}).startRTM();
+
+controller.hears(['order'], 'direct_message', function(bot, message) {
+	console.log(message);
+	bot.reply(message,
+		'Hi! I am helmut'
+	);
+    /*bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });*/
+});
