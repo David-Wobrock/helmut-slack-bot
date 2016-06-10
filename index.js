1d9de657d66e6039bbb1cc96af3800ecc6eba52a
mrWinston <marius.schulz.mail@gmail.com>
2016-06-10 18:02:25 +0200

    econd commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var rtmClient = require('@slack/client').RtmClient;
-var token = process.env.SLACK_API_TOKEN || '';
+var RtmClient = require('@slack/client').RtmClient;
+var token = 'xoxb-49895947781-WXerjwLI6CpltOcziC7RU6ze';
 var rtm = new RtmClient(token, {logLevel: 'debug'});
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
