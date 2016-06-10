45480c1475162e7ce8093699c56b7318f66af9a7
mrWinston <marius.schulz.mail@gmail.com>
2016-06-10 20:13:56 +0200

    some changes in structure,

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ function getId() {
 function createOrder(title) {
     var id = getId();
     orders[id] = {
+        'id': id,
         'title': title,
-        'targets': []
+        'targets': [],
+        'options': [],
     };
     return id;
 }
@@ -33,7 +35,7 @@ var Botkit = require('botkit');
 var os = require('os');
 
 var controller = Botkit.slackbot({
-    debug: true,
+    debug: false,
 });
 
 var bot = controller.spawn({
@@ -44,7 +46,6 @@ controller.hears(['order'], 'direct_message', function(bot, message) {
 	console.log(message);''
 
     bot.startPrivateConversation(message, function(err, conversation) {
-        message = message.replace('order', '').trim();
         console.log(message);
         // No message/title
         if (message === '') {
@@ -68,11 +69,21 @@ function askForTitle(conversation) {
 function askToMentionPeople(orderId, conversation) {
     conversation.ask("Mention all the people you will be able to take part of your order with @... @...", function(response, conversation) {
         console.log("You mentionend: " + response);
-        
-        var mentionedPersons = response.split(' ');
-        // TODO test that strings begin with @ (+ that this person exists?) 
+        if(response.text.trim().length==0){
+            conversation.say('You seem to ignore my question....');
+            askToMentionPeople(orderId, conversation);
+            return;
+        }
+        var mentionedPersons = response.text.split(' ');
+        // TODO test that strings begin with @ (+ that this person exists?)
+
+
         for (var i = 0; i < mentionedPersons.length; ++i)
-            orders[orderId]['targets'].append(mentionedPersons[i]);
+            if(!mentionedPersons[i].startsWith('<@')){
+                conversation.say('Sorry, we dont know who ' + mentionedPersons[i] + " is :(");
+                continue;
+            }
+            orders[orderId]['targets'].push(mentionedPersons[i]);
 
         askForPredefinedOption(orderId, conversation);
     });
@@ -80,15 +91,34 @@ function askToMentionPeople(orderId, conversation) {
 
 function askForPredefinedOption(orderId, conversation) {
     conversation.ask("Set a predefined option (or say finish)", function(response, conversation) {
-        if (response === 'finish') {
+        if (response.text === 'finish') {
             // Send to everyone
             var mentionendPersons = orders[orderId].targets;
             for (var i = 0; i < mentionendPersons; ++i) {
                 console.log("send to " + mentionendPersons[i]);
             }
         } else {
+            if(response.text.trim().length > 0){
+                orders[orderId].options.push(response);
+            }
             // Add predefined option in memory
             askForPredefinedOption(orderId, conversation);
         }
     });
+}
+
+
+function askOrder(orderId){
+    var currentOrder = orders[orderId];
+
+    for(var i = 0; i < currentOrder.targets.length; i++){
+        var uid = currentOrder.targets[i];
+        var owner = currentOrder.owner;
+        var choices = currentOrder.
+        bot.startPrivateConversation({'userId': currentOrder.targets[i] })
+    }
+}
+
+function getUserString(userid){
+    return "<@" + userid + ">";
 }
