18ebfee31af6e38c81d590f71b905d858aad1ab2
mrWinston <marius.schulz.mail@gmail.com>
2016-06-10 17:57:40 +0200

    INitial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Helmut Slack Bot
---
+++ index.js
@@ -0,0 +1,8 @@
+var rtmClient = require('@slack/client').RtmClient;
+var token = process.env.SLACK_API_TOKEN || '';
+var rtm = new RtmClient(token, {logLevel: 'debug'});
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+	console.log(message);
+});
+rtm.start();
